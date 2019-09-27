0bc136e137a121f00805dee8cf1acee858b0499c
Adrian Garcia <adriangarcia@javeriana.edu.co>
2019-09-27 17:05:17 -0500

    botones inflados en MainActivity

--- app/src/main/java/com/example/adoptapp/MainActivity.java
+++ app/src/main/java/com/example/adoptapp/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.adoptapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button botonIngresar;
+    Button botonRegistrarse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botonIngresar = findViewById(R.id.buttonIngresar);
+        botonRegistrarse = findViewById(R.id.buttonRegistrarse);
+
+        botonIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        botonRegistrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
