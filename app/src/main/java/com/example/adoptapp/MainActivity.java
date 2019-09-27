8b8586323ce46e4fcd0abc4aec8ca9a4a0b2bbe2
Adrian Garcia <adriangarcia@javeriana.edu.co>
2019-09-27 17:35:13 -0500

    creada actividad para login

--- app/src/main/java/com/example/adoptapp/MainActivity.java
+++ app/src/main/java/com/example/adoptapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.adoptapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,7 +23,8 @@ public class MainActivity extends AppCompatActivity {
         botonIngresar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(view.getContext(), ActivityLogin.class);
+                startActivity(intent);
             }
         });
 
