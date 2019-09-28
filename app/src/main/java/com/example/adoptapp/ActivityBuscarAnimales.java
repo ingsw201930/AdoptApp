762a2793413c4e719bbc1512df2cfe9b4abd39c1
Adrian Garcia <adriangarcia@javeriana.edu.co>
2019-09-27 21:56:32 -0500

    modificada la actividad de buscar animales

---
+++ app/src/main/java/com/example/adoptapp/ActivityBuscarAnimales.java
@@ -0,0 +1,14 @@
+package com.example.adoptapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityBuscarAnimales extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buscar_animales);
+    }
+}
