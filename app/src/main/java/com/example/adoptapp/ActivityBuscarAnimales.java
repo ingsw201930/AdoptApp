2e01456a3c318192f7ae54e343044c868747fc8b
Adrian Garcia <adriangarcia@javeriana.edu.co>
2019-10-03 21:42:55 -0500

    primera conexión con Firebase Firestore

--- app/src/main/java/com/example/adoptapp/ActivityBuscarAnimales.java
+++ app/src/main/java/com/example/adoptapp/ActivityBuscarAnimales.java
@@ -1,14 +1,44 @@
 package com.example.adoptapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class ActivityBuscarAnimales extends AppCompatActivity {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private static final String TAG = "MyActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buscar_animales);
+        traerListaAnimales();
+    }
+
+    public void traerListaAnimales(){
+
+        db.collection("animales")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
     }
 }
